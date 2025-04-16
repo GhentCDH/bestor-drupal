67a6ea59579bc3b724f9abf4d96745ba1dc1e662
hblomme <hblomme@UG-98V7XL3>
2025-04-16 17:28:23 +0200

    create and install empty bestor theme, a ruhi subtheme

---
+++ web/themes/custom/bestor/js/bestor.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * bestor behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bestor = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
