27530412ba4a003f709273f5d64d0d7a7ab83e13
hblomme <hblomme@UG-98V7XL3.localdomain>
2025-07-09 15:47:47 +0200

    custom config views

--- web/modules/custom/customviewfilters/src/Plugin/views/filter/CustomExtendedStringFilter.php
+++ web/modules/custom/customviewfilters/src/Plugin/views/filter/CustomExtendedStringFilter.php
@@ -3,6 +3,9 @@
 namespace Drupal\customviewfilters\Plugin\views\filter;
 
 use Drupal\config_views\Plugin\views\filter\StringEntity;
+use Drupal\relationship_nodes\Service\RelationshipInfoService;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
 
 /**
  * Provides an extended string filter with extra operators.
@@ -23,6 +26,12 @@ class CustomExtendedStringFilter extends StringEntity {
       'method' => 'opNotStartsWith',
       'values' => 1,
     ];
+    $operators['LINKED_TO_BUNDLE'] = [
+      'title' => $this->t('The vocabulary is linked to a bundle'),
+      'short' => $this->t('Give the machine name of the bundle (only applicable to a vid field)'),
+      'method' => 'linkedToBundle',
+      'values' => 1,
+    ];
     return $operators;
   }
 
@@ -37,14 +46,43 @@ class CustomExtendedStringFilter extends StringEntity {
     $query = $this->query;
     $helpquery = \Drupal::entityTypeManager()->getStorage('taxonomy_vocabulary')->getQuery();
 
-    $matching_ids = $helpquery
+    $matching_vids = $helpquery
     ->accessCheck(TRUE)
-    ->condition($field, $value, 'STARTS_WITH')// Zoek naar waarden die met $value beginnen')
+    ->condition($field, $value, 'STARTS_WITH')
     ->execute();
 
-    if (empty($matching_ids)) {
+    if (empty($matching_vids)) {
       return;
     }
-    $query->condition($this->options['group'], $field, $matching_ids, 'NOT IN');
+    $query->condition($this->options['group'], $field, $matching_vids, 'NOT IN');
+  }
+
+  protected function linkedToBundle($field) {
+    if($field != 'vid') {
+      return;
+    }
+    $bundle_info_service = \Drupal::service('entity_type.bundle.info')->getBundleInfo('node');
+    $value = $this->value;
+    if (empty($value) || !is_string($value) || !in_array($value, array_keys($bundle_info_service))) {
+      return;
+    }
+    $query = $this->query;
+    $bundle_fields = \Drupal::service('entity_field.manager')->getFieldDefinitions('node', $value);
+    $matching_vids = [];
+    foreach($bundle_fields as $bundle_field){
+      if($bundle_field->getType() == 'entity_reference' && $bundle_field->getSetting('target_type') == 'taxonomy_term') {
+        $matching_vids = array_merge($matching_vids, $bundle_field->getSettings()['handler_settings']['target_bundles']);
+      }
+    }
+
+    $relationship_nodes = \Drupal::service('relationship_nodes.relationship_info_service')->relationshipInfoForRelatedItemNodeType(\Drupal::entityTypeManager()->getDefinition('node'), $value);
+    if (is_array($relationship_nodes) && !empty($relationship_nodes)) {
+      foreach($relationship_nodes as $relationship_node) {
+        if (isset($relationship_node['relationtypeinfo']) && is_array($relationship_node['relationtypeinfo']) && isset($relationship_node['relationtypeinfo']['vocabulary']) && $relationship_node['relationtypeinfo']['vocabulary'] != ''){
+           $matching_vids[($relationship_node['relationtypeinfo']['vocabulary'])] = $relationship_node['relationtypeinfo']['vocabulary'];
+        }
+      }
+    }
+    $query->condition($this->options['group'], $field, array_unique($matching_vids), 'IN');
   }
 }
