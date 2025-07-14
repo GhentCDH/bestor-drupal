decc85b7af715eb52ad63ee97b58d77d183772b9
hblomme <hblomme@UG-98V7XL3.localdomain>
2025-07-14 18:04:49 +0200

    create custom dashboard for webmanagers

---
+++ web/modules/custom/customize_admin_menu_per_role/src/Controller/WebmanagerDashboardController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\customize_admin_menu_per_role\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+class WebmanagerDashboardController extends ControllerBase {
+
+  public function dashboard() {
+    $blocks = [];
+
+    // Content block
+    $blocks['content'] = [
+      '#theme' => 'admin_block',
+      '#attributes' => ['class' => ['admin-block', 'admin-block--content']],
+      '#block' => [
+        'title' => $this->t('Content'),
+        'content' => $this->buildLinks([
+          'Content overview' => 'view.content.page_2',
+          'Administer choicelists' => 'view.choice_lists.page_1',
+          'Update banners and block content' => 'view.contentblocks.page_1',
+          'All media' => 'entity.media.collection',
+          'Overview relationships' => 'view.content.page_3',
+          'Add content' => 'view.add_content.page_2',
+        ]),
+      ],
+    ];
+
+    // User management block
+    $blocks['users'] = [
+      '#theme' => 'admin_block',
+      '#attributes' => ['class' => ['admin-block', 'admin-block--users']],
+      '#block' => [
+        'title' => $this->t('User management'),
+        'content' => $this->buildLinks([
+          'Users' => 'entity.user.collection',
+        ]),
+      ],
+    ];
+
+    // Site config block
+    $blocks['config'] = [
+      '#theme' => 'admin_block',
+      '#attributes' => ['class' => ['admin-block', 'admin-block--config']],
+      '#block' => [
+        'title' => $this->t('Site configuration'),
+        'content' => $this->buildLinks([
+          'Site cache and performance' => 'system.performance_settings',
+          'Maintenance mode' => 'system.site_maintenance_mode',
+          'Regional settings' => 'system.regional_settings',
+          'User interface translation' => 'locale.translate_page',
+          'Configuration translation' => 'config_translation.mapper_list',
+          'URL aliases' => 'entity.path_alias.collection',
+        ]),
+      ],
+    ];
+
+    return [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['webmanager-dashboard', 'admin-blocks']],
+      'blocks' => $blocks,
+    ];
+  }
+
+  private function buildLinks(array $links): array {
+  $rendered = [];
+
+  foreach ($links as $title => $route) {
+    $link = Link::fromTextAndUrl($this->t($title), Url::fromRoute($route))->toRenderable();
+    $link['#attributes']['class'][] = 'admin-item__link';
+
+    // Wrap elke link in een div
+    $rendered[] = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['admin-item__title', 'custom-dashboard-link']],
+      'link' => $link,
+    ];
+  }
+
+  return $rendered;
+}
+}
---
+++ web/modules/custom/customize_admin_menu_per_role/src/EventSubscriber/AdminConfigRedirectSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\customize_admin_menu_per_role\EventSubscriber;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Path\PathMatcherInterface;
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class AdminConfigRedirectSubscriber implements EventSubscriberInterface {
+
+  protected $currentUser;
+  protected $pathMatcher;
+  protected $urlGenerator;
+
+  public function __construct(AccountInterface $current_user, PathMatcherInterface $path_matcher, UrlGeneratorInterface $url_generator) {
+    $this->currentUser = $current_user;
+    $this->pathMatcher = $path_matcher;
+    $this->urlGenerator = $url_generator;
+  }
+
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest', 31],
+    ];
+  }
+
+  public function onKernelRequest(RequestEvent $event) {
+    $request = $event->getRequest();
+    $path = $request->getPathInfo();
+
+    if ($path === '/admin') {
+      if (!\Drupal::currentUser()->hasPermission('administer site configuration') && \Drupal::currentUser()->hasPermission('access webmanager dashboard')) {
+        $url = $this->urlGenerator->generateFromRoute('customize_admin_menu_per_role.webmanager_dashboard');
+        $response = new RedirectResponse($url);
+        $event->setResponse($response);
+      }
+    }
+  }
+}
