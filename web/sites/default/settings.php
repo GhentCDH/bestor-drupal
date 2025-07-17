54602406cca5e64f1ec266e58487fb4825c15caa
Hans Blomme <hans.blomme@ugent.be>
2025-07-17 16:55:20 +0200

    disable auto translations in settings (generates errors)

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -931,3 +931,5 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
  }
 
+
+ $settings['locale_translation_auto_update'] = FALSE;
