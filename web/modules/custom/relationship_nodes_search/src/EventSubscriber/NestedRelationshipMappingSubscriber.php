e17d155736768b8f5cdb688381a3a349f365e632
Hans Blomme <hans.blomme@ugent.be>
2025-07-21 16:40:00 +0200

    continue wokring custom index field

---
+++ web/modules/custom/relationship_nodes_search/src/EventSubscriber/NestedRelationshipMappingSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\relationship_nodes_search\EventSubscriber;
+
+use Drupal\search_api_elasticsearch_client\Event\FieldMappingEvent;
+use Drupal\search_api_elasticsearch_client\Event\SupportsDataTypeEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class NestedRelationshipMappingSubscriber implements EventSubscriberInterface {
+
+  public static function getSubscribedEvents(): array {
+    return [
+        FieldMappingEvent::class => 'onFieldMapping',
+        SupportsDataTypeEvent::class => 'onSupportsDataType',
+    ];
+  }
+
+    public function onFieldMapping(FieldMappingEvent $event): void {
+    $field = $event->getField();
+    
+    if ($field->getType() === 'relationship_nodes_search_nested_relationship') {
+      dpm($field);
+
+      $event->setParam([
+        'type' => 'nested',
+       /* 'properties' => [
+          'id' => ['type' => 'keyword'],
+          'title' => ['type' => 'text'],
+        ],*/
+      ]);
+    }
+  }
+
+   public function onSupportsDataType(SupportsDataTypeEvent $event) {
+    $type = $event->getType();
+    if ($type === 'relationship_nodes_search_nested_relationship') {
+      $event->setIsSupported(TRUE);
+    }
+  }
+}
