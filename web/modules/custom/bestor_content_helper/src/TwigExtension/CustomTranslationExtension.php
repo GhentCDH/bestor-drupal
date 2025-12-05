c0d28594e0e3714f37b92ee5e8ecf417a3b5d910
Joren Six <joren.six@ugent.be>
2025-12-05 15:44:22 +0100

    chore: npm builds in docker file

---
+++ web/modules/custom/bestor_content_helper/src/TwigExtension/CustomTranslationExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\bestor_content_helper\TwigExtension;
+
+use Drupal\bestor_content_helper\Service\CustomTranslations;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Twig extension for custom translations.
+ */
+class CustomTranslationExtension extends AbstractExtension {
+
+  protected LanguageManagerInterface $languageManager;
+  protected CustomTranslations $customTranslations;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(LanguageManagerInterface $languageManager, CustomTranslations $customTranslations) {
+    $this->languageManager = $languageManager;
+    $this->customTranslations = $customTranslations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFunctions(): array {
+    return [
+      new TwigFunction('bestor', [$this, 'translate'], [
+        'is_safe' => ['html'],
+      ]),
+    ];
+  }
+
+
+  /**
+   * Get custom translation.
+   */
+  public function translate(string $key, ?string $langcode = NULL): Markup {
+    if($langcode == NULL){
+      $langcode = $this->languageManager->getCurrentLanguage()->getId();
+    }
+    if($key === 'banner_subtitle'){
+        return Markup::create($this->customTranslations->generateBannerSubtitleHtml($langcode));
+    }
+    return Markup::create($this->customTranslations->get($key, $langcode));
+  }
+}
