e300b07c322d3652bb0fda0218f99c7869332f33
Hans Blomme <hans.blomme@ugent.be>
2026-09-16 16:29:10 +0200

    chore: update searchbanner tagline config

--- web/modules/custom/bestor_content_helper/src/Service/SiteSettingManager.php
+++ web/modules/custom/bestor_content_helper/src/Service/SiteSettingManager.php
@@ -5,6 +5,7 @@ namespace Drupal\bestor_content_helper\Service;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\bestor_content_helper\Service\UrlProvider;
 use Drupal\Core\Render\Markup;
+use Drupal\Component\Utility\Html;
 
 
 /* Service to ease the communication with the custom entity type BestorSiteSetting*/
@@ -38,15 +39,55 @@ class SiteSettingManager {
 
 
   public function getSearchTagline(): ?Markup {
-    $prefix = $this->getBestorSiteSetting('searchbanner_subtitle_prefix');
-    $linktext = $this->getBestorSiteSetting('searchbanner_subtitle_link');
-    $suffix = $this->getBestorSiteSetting('searchbanner_subtitle_suffix');
-
-    if ($linktext) {
-      $url = $this->urlProvider->getTranslatedUrlFromRoute('view.database.page_1')->toString();
-      $linktext = '<a href="' . $url . '">' . $linktext . '</a>';
+    $raw = $this->getBestorSiteSetting('searchbanner_tagline');
+    if (!$raw) {
+      return NULL;
     }
 
-    return Markup::create(implode(' ', array_filter([$prefix, $linktext, $suffix])));
+    $search_url = $this->urlProvider->getTranslatedUrlFromRoute('view.database.page_1')->toString();
+    $analysis_url = $this->urlProvider->getTranslatedUrlFromRoute('view.database_advanced_search.page_6')->toString();
+    $urls = [$search_url, $analysis_url];
+
+    $text = Html::escape($raw);
+    $linked = $this->parseBracketLinks($text, $urls);
+    return Markup::create($linked);
+  }
+ 
+
+  private function parseBracketLinks(string $text, array $urls): string {
+    // look for first '['.
+    $open1 = strpos($text, '[');
+    if ($open1 === FALSE) {
+      return $text;
+    }
+
+    // look for closing ']'.
+    $close1 = strpos($text, ']', $open1 + 1);
+    if ($close1 === FALSE) {
+      return $text;
+    }
+
+    $before = substr($text, 0, $open1);
+    $linktext1 = substr($text, $open1 + 1, $close1 - $open1 - 1);
+    $result = $before . '<a href="' . $urls[0] . '">' . $linktext1 . '</a>';
+    $pos = $close1 + 1;
+
+    // look for second '[' after first closing.
+    $open2 = strpos($text, '[', $pos);
+    if ($open2 === FALSE) {
+      return $result . substr($text, $pos);
+    }
+
+    $close2 = strpos($text, ']', $open2 + 1);
+    if ($close2 === FALSE) {
+      return $result . substr($text, $pos);
+    }
+
+    $between = substr($text, $pos, $open2 - $pos);
+    $linktext2 = substr($text, $open2 + 1, $close2 - $open2 - 1);
+    $result .= $between . '<a href="' . $urls[1] . '">' . $linktext2 . '</a>';
+
+    // anything after second closing will be displayed as is.
+    return $result . substr($text, $close2 + 1);
   }
 }
